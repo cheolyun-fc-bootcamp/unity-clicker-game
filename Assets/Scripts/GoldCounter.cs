d7feba2b8349d3be1c6b30dd30ef7644e53db13b
Cheolyun <philoethic@Jeong-ui-MacBook-Air.local>
2017-11-25 18:11:30 +0900

    1주차 수업 끝

---
+++ Assets/Scripts/GoldCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldCounter : MonoBehaviour {
+	public int Gold = 0;
+	public Text TextGold;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (StartGoldCounter());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator StartGoldCounter(){
+		while (true) {
+			yield return new WaitForSecondsRealtime (1f); // wait 1s each roop
+			Gold++;
+			TextGold.text = Gold.ToString ();
+		}
+	}
+}
